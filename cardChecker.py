78fdf06da4205590e7c0be5907b7541d24f23a27
Duc Bui <buianhduc.popodoo2@gmail.com>
2020-09-21 00:16:58 +0700

    Create cardChecker.py

---
+++ cardChecker.py
@@ -0,0 +1,21 @@
+def LuhnAlgorithm(creditCard):
+    nDigits = len(creditCard)
+    nSum = 0;
+    isSecond = False
+    for i in range(len(creditCard)-1,-1,-1):
+        d = int(creditCard[i])
+
+        if isSecond:
+            d *= 2
+        
+        nSum += d // 10
+        nSum += d % 10
+
+        isSecond = not isSecond
+    return (nSum % 10 == 0)
+
+creditCard = input()
+if(LuhnAlgorithm(creditCard)):
+    print("yesssss")
+else:
+    print("fuckk you")
